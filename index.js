06d8439c48babdddb7ae7900b76a9c68ebc5fc66
Vishal Dave <49125707+vishald1994@users.noreply.github.com>
2019-09-15 11:38:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ function enterPressed(event) {
   }
 }
 
+function cToF(celsius){
+  var cTemp = celsius;
+  var cToFahr = cTemp * 9 / 5 + 32;
+    return cToFahr;
+}
+
 function findWeatherDetails() {
   if (searchInput.value === "") {
   }
@@ -28,7 +34,7 @@ function theResponse(response) {
   let jsonObject = JSON.parse(response);
   cityName.innerHTML = jsonObject.name;
   icon.src = "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
-  temperature.innerHTML = parseInt(jsonObject.main.temp - 273) + "°";
+  temperature.innerHTML = parseInt(cToF(jsonObject.main.temp - 273)) + "°";
   humidity.innerHTML = jsonObject.main.humidity + "%";
 }
 
@@ -36,10 +42,10 @@ function httpRequestAsync(url, callback)
 {
   console.log("hello");
     var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = () => { 
+    httpRequest.onreadystatechange = () => {
         if (httpRequest.readyState == 4 && httpRequest.status == 200)
             callback(httpRequest.responseText);
     }
-    httpRequest.open("GET", url, true); // true for asynchronous 
+    httpRequest.open("GET", url, true); // true for asynchronous
     httpRequest.send();
 }
